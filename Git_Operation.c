931325468cb5bb2ca5e8e0a9ad4f3765e30c103a
cw <1509832650@qq.com>
2022-08-05 22:08:28 +0800

    第三次提交

--- Git_Operation.c
+++ Git_Operation.c
@@ -13,7 +13,19 @@ git init
 git add
 
 提交文件到本地版本库
-git commit
+git commit -m "添加关于提交的说明信息"
 
 查看提交记录
 git log
+
+硬回退, 注意此操作c, 会将比目标硬回退版本号更新的版本, 全部删除. 所以需要谨慎使用.
+git reset --hard 提交Id
+
+创建分支
+git branch 分支名
+
+切换分支
+git checkout 分支名
+
+在分支A上, 把分支B合并到分支A上
+git merge 被合并分支名
