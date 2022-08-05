477a21ae9b2dbf6a72a4d5dd76eee0c46f5e2675
cw <1509832650@qq.com>
2022-08-05 21:46:54 +0800

    第一次提交-2022.08.5

---
+++ Git_Operation.c
@@ -0,0 +1,16 @@
+
+查看Git版本, 确定当前电脑是否安装Git
+git version
+
+设置用户名和邮箱
+git config --global user.name "yourname"
+git config --global user.email "1509832650@qq.com"
+
+在当前目录初始化生成一个空的Git仓库, 表现为生成一个名为.git的隐藏文件夹
+git init
+
+添加文件到暂存区
+git add
+
+提交文件到本地版本库
+git commit
